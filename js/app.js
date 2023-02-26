148174acd47e02ffe020e6685abd8c4c7cb5ac22
David Jon Oswald II <doswald27@gmail.com>
2023-02-26 17:23:36 +0000

    finished problem 4

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function sum(a, b) {
   return [mySum,myString];
 }
 //Working on problem 1 
-// Here is the test for sum(); uncomment it to run it
+//Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -88,12 +88,17 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  let arraySumA = sum(sumArr[0], sumArr[1]);
+  let arraySumB = sum(arraySumA[0], arraySumA[1]);
 
+  let arrayString = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${arraySumB[0]} is their sum.`;
+
+  return [arraySumB[0], arrayString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
